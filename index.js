2453463309ea879cf601993ba86190be2f6002ec
GabedeSousa <gabriel.fidelis.sousa@gmail.com>
2020-06-03 14:00:35 -0700

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,62 @@
 const driver = {}
 
 function updateDriverWithKeyAndValue(driver, key, value){
- return Object.assign({}, driver,{[key]: value})
+
+  return  Object.assign({}, driver, {[key]:value})
+ 
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+
+    return Object.assign(driver, {[key]:value})
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-    driver[key] = value;
-    return driver;
-}
 
 function deleteFromDriverByKey(driver, key){
- let driver1 = Object.assign({}, driver)
-
-  delete driver1[key]
-
-  return driver1
+  let driver1 =   Object.assign({}, driver)
+    delete driver1[key]
+    return driver1
 }
+
 
 function destructivelyDeleteFromDriverByKey(driver, key){
-    const driver3 = delete driver[key]
-
-    return driver
+        const driver3 = delete driver[key]
+    
+        return driver
 }
+
+
+
+
+
+
+
+// function updateDriverWithKeyAndValue(driver, key, value){
+//  return Object.assign({}, driver,{[key]: value})
+// }
+
+// function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
+//     driver[key] = value;
+//     return driver;
+// }
+
+// function deleteFromDriverByKey(driver, key){
+//  let driver1 = Object.assign({}, driver)
+
+//   delete driver1[key]
+
+//   return driver1
+// }
+
+// function destructivelyDeleteFromDriverByKey(driver, key){
+//     const driver3 = delete driver[key]
+
+//     return driver
+// }
+
+
+
+
+
+
+
+
